2d18a3e6a90ddb590427f35f9de29613c339bf3e
ScriptCodes <143606581+ScriptCodes@users.noreply.github.com>
2024-04-03 11:24:50 +0200

    alhamdulillah

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -8,7 +8,7 @@ import io.javalin.rendering.template.JavalinThymeleaf;
 public class Main {
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres";
-    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String URL = "jdbc:postgresql://localhost:5433/%s?currentSchema=public";
     private static final String DB = "cupcake";
 
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
