b30533a158f1cd83dbcc65c69ed4667304f233d8
Mattsigurd <matthias.sigurds@gmail.com>
2024-04-03 11:54:55 +0200

    login+create user

---
+++ src/main/java/app/entities/User.java
@@ -0,0 +1,49 @@
+package app.entities;
+
+public class User
+{
+    private int userId;
+    private String userName;
+    private String password;
+    private boolean admin;
+
+    public User(int userId, String userName, String password, String role)
+    {
+        this.userId = userId;
+        this.userName = userName;
+        this.password = password;
+        this.admin = admin;
+    }
+
+
+    public int getUserId()
+    {
+        return userId;
+    }
+
+    public String getUserName()
+    {
+        return userName;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public boolean getAdmin()
+    {
+        return admin;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "User{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + admin + '\'' +
+                '}';
+    }
+}
