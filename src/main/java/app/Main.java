64ee88080e15288b90987594d86ff700b2aff248
Rouvii <115996187+Rouvii@users.noreply.github.com>
2024-04-05 14:08:13 +0200

    delete button order + orderline

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class Main {
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres";
-    private static final String URL = "jdbc:postgresql://localhost:5433/%s?currentSchema=public";
+    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
     private static final String DB = "cupcake";
 
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
